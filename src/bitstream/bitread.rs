4d63176946957f89a1bbc03b7f31897433955971
Luca Barbato <lu_zero@gentoo.org>
2016-01-31 17:06:17 +0100

    bitstream: First mock function

---
+++ src/bitstream/bitread.rs
@@ -0,0 +1,52 @@
+pub trait BitRead {
+    fn get_bit1(&mut self) -> bool;
+/*
+    fn peek_bit1(size : u8) -> bool;
+
+    fn get_bits32(size : u8) -> u32;
+
+    fn get_bits64(size : u8) -> u64;
+
+    fn peek_bits32(size : u8) -> u32;
+
+    fn peek_bits64(size : u8) -> u64;
+
+    fn skip_bits(size : u8) -> ();
+*/
+}
+
+pub struct LittleEndian <'a> {
+    buffer : &'a[u8],
+    index : usize,
+    cache : u64,
+    left : usize,
+}
+
+impl <'a> BitRead for LittleEndian<'a> {
+    fn get_bit1(&mut self) -> bool {
+        let val = (self.cache & 1) == 1;
+        self.cache = self.cache >> 1;
+        return val;
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+
+#[test]
+    fn get_bit1() {
+        let mut reader = LittleEndian {
+            buffer : &b""[..],
+            index : 0,
+            cache: 0b10,
+            left: 2
+        };
+
+        assert!(!reader.get_bit1());
+        assert!(reader.get_bit1());
+    }
+
+}
