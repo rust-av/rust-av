3e2ac5f7a6fb0d5c5d320960eb67df50de022d7f
Luca Barbato <lu_zero@gentoo.org>
2016-01-31 01:12:36 +0100

    More Scaffold

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+// core functionalities
+mod bitstream;
+mod entropy;
+mod data;
+mod io;
+
+// encoded data manipulation
+mod parser;
+mod codec;
+mod format;
+
+// raw multimedia data manipulation
+mod filter;
+mod resample;
+mod scale;
+
 #[test]
 fn it_works() {
 }
