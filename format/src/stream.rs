7ec963056387d60e01638a3402468fc34a2630bf
Geoffroy Couprie <geo.couprie@gmail.com>
2017-09-13 14:48:41 +0200

    stream struct members should be public

--- format/src/stream.rs
+++ format/src/stream.rs
@@ -4,37 +4,37 @@ use rational::Rational32;
 use data::frame::MediaKind;
 
 #[derive(Clone,Debug,PartialEq)]
-enum CodecID {
+pub enum CodecID {
     VP9,
     Opus
 }
 
 #[derive(Clone,Debug,PartialEq)]
 pub struct CodecParams {
-    kind: MediaKind,
-    codec_id: CodecID,
-    extradata: Vec<u8>,
-    tag: u32,
-    bit_rate: usize,
-    bits_per_coded_sample: usize,
-    profile: usize,
-    level: usize,
+    pub kind: MediaKind,
+    pub codec_id: CodecID,
+    pub extradata: Vec<u8>,
+    pub tag: u32,
+    pub bit_rate: usize,
+    pub bits_per_coded_sample: usize,
+    pub profile: usize,
+    pub level: usize,
 }
 
 #[derive(Clone,Debug,PartialEq)]
 pub struct Stream {
-    id: usize,
-    index: usize,
-    params : CodecParams,
-    start: Option<u64>,
-    duration: Option<u64>,
-    timebase : Rational32,
+    pub id: usize,
+    pub index: usize,
+    pub params : CodecParams,
+    pub start: Option<u64>,
+    pub duration: Option<u64>,
+    pub timebase : Rational32,
 //  seek_index : SeekIndex
 }
 
 pub struct StreamGroup<'a> {
-    id: usize,
-    start: u64,
-    end: u64,
-    streams: &'a [Stream],
+    pub id: usize,
+    pub start: u64,
+    pub end: u64,
+    pub streams: &'a [Stream],
 }
