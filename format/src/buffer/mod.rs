ea3b320ce74a12c901698653c8c8e8a837fe64a0
Geoffroy Couprie <geo.couprie@gmail.com>
2017-09-13 14:48:23 +0200

    implement Buffered for Cursor

--- format/src/buffer/mod.rs
+++ format/src/buffer/mod.rs
@@ -2,8 +2,14 @@ mod accreader;
 
 pub use self::accreader::AccReader;
 
-use std::io::{BufRead, Seek};
+use std::io::{BufRead, Seek, Cursor};
 
 pub trait Buffered: BufRead + Seek {
     fn data(&self) -> &[u8];
+}
+
+impl<'a> Buffered for Cursor<&'a [u8]> {
+    fn data(&self) -> &[u8] {
+        &self.get_ref()[self.position() as usize..]
+    }
 }
