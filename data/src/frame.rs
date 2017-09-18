d22f32929beed82d692b9bcae12083624c54feae
Luca Barbato <lu_zero@gentoo.org>
2017-09-18 23:13:42 +0200

    wip: Add Frame copy_from functions
    TODO: move it in a separate trait and possibly crate later.

--- data/src/frame.rs
+++ data/src/frame.rs
@@ -146,3 +146,142 @@ pub fn new_default_frame(kind: &MediaKind, t: Option<TimeInfo>) -> Frame {
         t: t,
     }
 }
+
+use std::ptr::copy_nonoverlapping;
+
+fn copy_plane(
+    dst: &mut [u8],
+    dst_linesize: usize,
+    src: &[u8],
+    src_linesize: usize,
+    w: usize,
+    h: usize,
+) {
+    let dst_chunks = dst.chunks_mut(dst_linesize);
+    let src_chunks = src.chunks(src_linesize);
+
+    for (d, s) in dst_chunks.zip(src_chunks).take(h) {
+        unsafe {
+            copy_nonoverlapping(s.as_ptr(), d.as_mut_ptr(), w);
+        }
+    }
+}
+
+fn copy_image<'a, IM, IU, I>(
+    dst: IM,
+    dst_linesizes: IU,
+    src: I,
+    src_linesizes: IU,
+    w: usize,
+    h: usize,
+    fmt: &Formaton,
+) where
+    IM: Iterator<Item = &'a mut [u8]>,
+    I: Iterator<Item = &'a [u8]>,
+    IU: Iterator<Item = usize>,
+{
+    let dst_iter = dst.zip(dst_linesizes);
+    let src_iter = src.zip(src_linesizes);
+    let iter = dst_iter.zip(src_iter).zip(fmt.iter());
+
+    for (((d, d_linesize), (s, s_linesize)), c) in iter {
+        copy_plane(
+            d,
+            d_linesize,
+            s,
+            s_linesize,
+            c.unwrap().get_width(w),
+            c.unwrap().get_height(h),
+        );
+    }
+}
+
+// TODO: Add proper tests
+fn copy_to_frame<'a, I, IU>(dst: &mut Frame, mut src: I, mut src_linesize: IU, w: usize, h: usize)
+where
+    I: Iterator<Item = &'a [u8]>,
+    IU: Iterator<Item = usize>,
+{
+    if let MediaKind::Video(ref fmt) = dst.kind {
+        let mut f_iter = fmt.format.iter();
+        let w = fmt.width;
+        let h = fmt.height;
+        for i in 0..dst.buf.count() {
+            let d_linesize = dst.buf.linesize(i).unwrap();
+            let s_linesize = src_linesize.next().unwrap();
+            let d = dst.buf.as_mut_slice(i).unwrap();
+            let s = src.next().unwrap();
+            let c = f_iter.next().unwrap();
+            copy_plane(
+                d,
+                d_linesize,
+                s,
+                s_linesize,
+                c.unwrap().get_width(w),
+                c.unwrap().get_height(h),
+            );
+        }
+    } else {
+        unimplemented!();
+    }
+}
+
+use std::mem;
+use std::slice;
+
+// TODO make it a separate trait
+impl Frame {
+    fn copy_from_slice<'a, I, IU>(&mut self, mut src: I, mut src_linesize: IU)
+    where
+        I: Iterator<Item = &'a [u8]>,
+        IU: Iterator<Item = usize>,
+    {
+        if let MediaKind::Video(ref fmt) = self.kind {
+            let mut f_iter = fmt.format.iter();
+            let w = fmt.width;
+            let h = fmt.height;
+            for i in 0..self.buf.count() {
+                let d_linesize = self.buf.linesize(i).unwrap();
+                let s_linesize = src_linesize.next().unwrap();
+                let d = self.buf.as_mut_slice(i).unwrap();
+                let s = src.next().unwrap();
+                let c = f_iter.next().unwrap();
+                copy_plane(
+                    d,
+                    d_linesize,
+                    s,
+                    s_linesize,
+                    c.unwrap().get_width(w),
+                    c.unwrap().get_height(h),
+                );
+            }
+        } else {
+            unimplemented!();
+        }
+    }
+
+    fn copy_from_raw_parts<'a, I, IU>(&mut self, mut src: I, mut src_linesize: IU)
+    where
+        I: Iterator<Item = *const u8>,
+        IU: Iterator<Item = usize>,
+    {
+        if let MediaKind::Video(ref fmt) = self.kind {
+            let mut f_iter = fmt.format.iter();
+            let w = fmt.width;
+            let h = fmt.height;
+            for i in 0..self.buf.count() {
+                let d_linesize = self.buf.linesize(i).unwrap();
+                let s_linesize = src_linesize.next().unwrap();
+                let d = self.buf.as_mut_slice(i).unwrap();
+                let c = f_iter.next().unwrap();
+                let r = src.next().unwrap();
+                let hb = c.unwrap().get_height(h);
+                let s = unsafe { slice::from_raw_parts(r, hb * s_linesize) };
+                copy_plane(d, d_linesize, s, s_linesize, c.unwrap().get_width(w), hb);
+                mem::forget(s);
+            }
+        } else {
+            unimplemented!();
+        }
+    }
+}
