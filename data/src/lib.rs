55139abf617620b5508f08be6968dd1426d9c648
Luca Barbato <lu_zero@gentoo.org>
2017-10-09 19:06:58 +0200

    Add Value type
    Will be used to pass optional values.

--- data/src/lib.rs
+++ data/src/lib.rs
@@ -23,3 +23,4 @@ pub mod frame;
 pub mod packet;
 pub mod pixel;
 pub mod timeinfo;
+pub mod value;
---
+++ data/src/value.rs
@@ -0,0 +1,66 @@
+use pixel::Formaton;
+use audiosample::Soniton;
+
+use std::rc::Rc;
+use std::convert::From;
+
+#[derive(Debug)]
+pub enum Value<'a> {
+    I64(i64),
+    U64(u64),
+    Str(&'a str),
+    Bool(bool),
+    Formaton(Rc<Formaton>),
+    Soniton(Rc<Soniton>),
+}
+
+impl<'a> From<i64> for Value<'a> {
+    fn from(v: i64) -> Self {
+        Value::I64(v)
+    }
+}
+
+impl<'a> From<u64> for Value<'a> {
+    fn from(v: u64) -> Self {
+        Value::U64(v)
+    }
+}
+
+impl<'a> From<&'a str> for Value<'a> {
+    fn from(v: &'a str) -> Self {
+        Value::Str(v)
+    }
+}
+
+impl<'a> From<bool> for Value<'a> {
+    fn from(v: bool) -> Self {
+        Value::Bool(v)
+    }
+}
+
+impl<'a> From<Rc<Formaton>> for Value<'a> {
+    fn from(v: Rc<Formaton>) -> Self {
+        Value::Formaton(v)
+    }
+}
+
+impl<'a> From<Rc<Soniton>> for Value<'a> {
+    fn from(v: Rc<Soniton>) -> Self {
+        Value::Soniton(v)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::fmt::Debug;
+
+    fn p<'a, T>(v: T) where T: Into<Value<'a>>+Debug {
+        println!("{:?}", v);
+    }
+
+    #[test]
+    fn value_str() {
+        p("test");
+    }
+}
