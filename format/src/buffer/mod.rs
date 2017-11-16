546a92acee0b6cf209f85e7dbebcbcb4135526b4
Luca Barbato <lu_zero@gentoo.org>
2017-11-16 06:27:44 +0100

    format: Drop the easy Buffered implementation for Cursor

--- format/src/buffer/mod.rs
+++ format/src/buffer/mod.rs
@@ -7,9 +7,3 @@ use std::io::{BufRead, Seek, Cursor};
 pub trait Buffered: BufRead + Seek {
     fn data(&self) -> &[u8];
 }
-
-impl<'a> Buffered for Cursor<&'a [u8]> {
-    fn data(&self) -> &[u8] {
-        &self.get_ref()[self.position() as usize..]
-    }
-}
