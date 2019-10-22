58e634360a92d2f65a88e1c5af5d7b664fbc2efb
xd009642 <danielmckenna93@gmail.com>
2019-10-22 23:01:11 +0100

    Add test for error conversion
    As conversion is now automatically derived added a test to ensure this remains correct

--- format/src/error.rs
+++ format/src/error.rs
@@ -11,3 +11,20 @@ pub enum Error {
 }
 
 pub type Result<T> = ::std::result::Result<T, Error>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn io_error_conversion() {
+        let io_err = io::Error::new(io::ErrorKind::Other, "foobar");
+
+        let err: Error = io_err.into();
+        
+        match err {
+            Error::Io(_) => {},
+            _ => panic!("Error doesn't match"),
+        }
+    }
+}
