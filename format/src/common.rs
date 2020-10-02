1dd59e89fd3f5f82c1d3851e8aae1fc4e7bceb01
Luni-4 <luni-4@hotmail.it>
2020-10-02 17:56:51 +0200

    Document the format crate

--- format/src/common.rs
+++ format/src/common.rs
@@ -1,14 +1,23 @@
 use crate::data::rational::Rational64;
 use crate::stream::Stream;
 
+/// Global media file information.
 #[derive(Debug, Clone)]
 pub struct GlobalInfo {
+    /// Duration of a media file.
+    ///
+    /// If `None`, the duration of a media file is not considered.
     pub duration: Option<u64>,
+    /// Timebase associated to a media file.
+    ///
+    /// If `None`, the timebase of a media file is not considered.
     pub timebase: Option<Rational64>,
+    /// List of streams present in a media file.
     pub streams: Vec<Stream>,
 }
 
 impl GlobalInfo {
+    /// Adds a stream to the list of streams present in a media file.
     pub fn add_stream(&mut self, mut st: Stream) -> usize {
         let idx = self.streams.len();
 
--- format/src/error.rs
+++ format/src/error.rs
@@ -2,16 +2,21 @@ use std::io;
 
 use thiserror::Error;
 
+/// General muxing/demuxing errors.
 #[derive(Debug, Error)]
 pub enum Error {
+    /// Invalid input data.
     #[error("Invalid Data")]
     InvalidData,
+    /// A muxing/demuxing operation needs more data to be completed.
     #[error("{0} more bytes needed")]
     MoreDataNeeded(usize),
     #[error("I/O error")]
+    /// A more generic I/O error.
     Io(#[from] io::Error),
 }
 
+/// A specialised `Result` type for muxing/demuxing operations.
 pub type Result<T> = ::std::result::Result<T, Error>;
 
 #[cfg(test)]
