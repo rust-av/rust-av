0c3ebfaf0dcc91d32f5bd445e2a20a75df9394df
Luni-4 <luni-4@hotmail.it>
2021-05-21 16:05:14 +0200

    format: Add Sync trait to Buffered trait

--- format/src/buffer/mod.rs
+++ format/src/buffer/mod.rs
@@ -5,7 +5,7 @@ pub use self::accreader::AccReader;
 use std::io::{BufRead, Seek};
 
 /// Used to interact with a buffer.
-pub trait Buffered: BufRead + Seek + Send {
+pub trait Buffered: BufRead + Seek + Send + Sync {
     /// Returns the data contained in a buffer as a sequence of bytes.
     fn data(&self) -> &[u8];
     /// Increases the size of a buffer.
